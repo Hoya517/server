8caa95384477525f647be577e75e8002306b1cfe
hoya <sv05159@naver.com>
2023-03-13 23:01:12 +0900

    서블릿 컨테이너 초기화2
    [서블릿을 등록하는 2가지 방법]
    1. @WebServlet
    2. 프로그래밍 방식 -> 1)어노테이션 방식과는 다르게 변경에 유연함.
    
    [초기화]
    1. 서블릿 컨테이너 초기화 실행
      - resources/META-INF/services/jakarta.servlet.ServletContainerInitializer
    2. 애플리케이션 초기화 실행 -> 편리, 의존(유연)
      - @HandlesTypes(AppInit.class)

--- src/main/java/hello/container/MyContainerInitV1.java
+++ src/main/java/hello/container/MyContainerInitV1.java
@@ -9,8 +9,8 @@ import java.util.Set;
 public class MyContainerInitV1 implements ServletContainerInitializer {
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        System.out.println("MyContainerInitV1.onStartup");
-        System.out.println("MyContainerInitV1 c = " + c);
-        System.out.println("MyContainerInitV1 ctx = " + ctx);
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("MyContainerInitV2 c = " + c);
+        System.out.println("MyContainerInitV2 ctx = " + ctx);
     }
 }
